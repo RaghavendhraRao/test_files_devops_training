1d28a298c02179fe8618900f35c7eef74c759530
Raghavendhra-herts <rd22aak@herts.ac.uk>
2024-10-19 19:43:41 +0100

    new file

---
+++ testing_file2.py
@@ -0,0 +1,17 @@
+print("testing another file")
+print("this is a python file")
+
+print("vagrant---> install linux system (2 different linux systems(ubuntu1 & ubuntu2))
+			install the following in linux systems
+			java
+			tomcat
+			maven
+		install jenkins in local system
+
+		jenkins
+		create new node(nodel)
+		assign one linux system to new node(node1)")
+		
+	
+
+
